099534316cda463814e7bc829140c3013f11f4e6
heltid <aiga.kalneja@gmail.com>
2021-11-25 23:41:09 +0100

    Made net and watering can work

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 //MAKE THE MAGIC HAPPEN
+
+// Net
+var timesPerSecond = 5;
+var wait = false;
+$(document).mousemove(function(e){
+    if (!wait) {
+        $("#net").css({left:e.pageX, top:e.pageY});
+        wait = true;
+        setTimeout(function () {
+            wait = false;
+        }, 1000 / timesPerSecond);
+    } 
+});
+
+//Watering can 
+
+$('.watercan').on('click', function (event) {
+    $('.wateringcan').toggleClass('rotate');
+    $('.wateringcan').toggleClass('rotate-reset');
+});
