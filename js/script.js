55929cebc548c79889cdae8de1b68c9301f64fb9
fjbass <vladprodan2000@yahoo.com>
2021-11-26 00:54:06 +0100

    add droplet activity

--- js/script.js
+++ js/script.js
@@ -3,19 +3,25 @@
 // Net
 var timesPerSecond = 5;
 var wait = false;
-$(document).mousemove(function(e){
+$(document).mousemove(function (e) {
     if (!wait) {
-        $("#net").css({left:e.pageX, top:e.pageY});
+        $("#net").css({ left: e.pageX, top: e.pageY });
         wait = true;
         setTimeout(function () {
             wait = false;
         }, 1000 / timesPerSecond);
-    } 
+    }
 });
 
 //Watering can 
-
 $('.watercan').on('click', function (event) {
     $('.wateringcan').toggleClass('rotate');
     $('.wateringcan').toggleClass('rotate-reset');
+});
+
+//waterdrops
+let a = false;
+$('.watercan').on('click', function (event) {
+    a = !a;
+    a ? $('.waterdrop').css("visibility", "visible") : $('.waterdrop').css("visibility", "hidden");
 });
